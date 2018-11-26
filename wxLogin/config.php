0662f1deecd0d1a4ea3f39649d0680c75210bf36
Tong <yiyecao1993@gmail.com>
2018-11-26 14:04:00 +0800

    Update config.php

--- wxLogin/config.php
+++ wxLogin/config.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
-	define('appID', 'wx1540badb21525e68');
-	define('appSecret', 'fdc3c2f2c12d5a1672b0e071b2c89328');
-	define('BACKURL', 'http://www.mogucode.top:8083/Mogu/wxLogin/callback.php');
+	define('appID', 'xxx');
+	define('appSecret', 'xxxx');
+	define('BACKURL', 'http://www.xxx.xxx:8083/Mogu/wxLogin/callback.php');
 	define('tokenFile', 'access_token.json');
 ?>
